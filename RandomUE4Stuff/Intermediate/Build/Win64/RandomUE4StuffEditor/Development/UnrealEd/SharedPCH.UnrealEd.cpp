00d93fe12f1bf00df49fabbb6f7d22bfb9fb6fcb
Sage_of_Mirrors <empressglatia@gmail.com>
2020-02-10 23:48:19 -0600

    Progress on animation. Created a prototype for the Yawn state machine.

--- RandomUE4Stuff/Intermediate/Build/Win64/RandomUE4StuffEditor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
+++ RandomUE4Stuff/Intermediate/Build/Win64/RandomUE4StuffEditor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
@@ -1,1 +1,1 @@
-#include "C:/Github/RandomUE4Stuff/RandomUE4Stuff/Intermediate/Build/Win64/RandomUE4StuffEditor/Development/UnrealEd/SharedPCH.UnrealEd.h"
+#include "D:/Github/RandomUE4Stuff/RandomUE4Stuff/Intermediate/Build/Win64/RandomUE4StuffEditor/Development/UnrealEd/SharedPCH.UnrealEd.h"
--- RandomUE4Stuff/Intermediate/Build/Win64/RandomUE4StuffEditor/Development/UnrealEd/SharedPCH.UnrealEd.h
+++ RandomUE4Stuff/Intermediate/Build/Win64/RandomUE4StuffEditor/Development/UnrealEd/SharedPCH.UnrealEd.h
@@ -1,4 +1,4 @@
-// PCH for C:\Program Files\Epic Games\UE_4.23\Engine\Source\Editor\UnrealEd\Public\UnrealEdSharedPCH.h
+// PCH for D:\Program Files\Epic Games\UE_4.23\Engine\Source\Editor\UnrealEd\Public\UnrealEdSharedPCH.h
 #define IS_PROGRAM 0
 #define UE_EDITOR 1
 #define ENABLE_PGO_PROFILE 0
@@ -374,4 +374,4 @@
 #define WITH_GAMEPLAY_DEBUGGER 1
 #define AIMODULE_VTABLE DLLIMPORT_VTABLE
 #define AIMODULE_API DLLIMPORT
-#include "C:/Program Files/Epic Games/UE_4.23/Engine/Source/Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
+#include "D:/Program Files/Epic Games/UE_4.23/Engine/Source/Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
